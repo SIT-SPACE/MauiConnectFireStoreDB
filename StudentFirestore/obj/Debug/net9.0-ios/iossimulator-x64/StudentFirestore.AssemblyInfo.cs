251204f3de02ec773d3fe30f27d389ccbe4495cc
SiT <Phisitphon7648@gmail.com>
2024-11-21 23:46:57 +0700

    Firestore

--- StudentFirestore/obj/Debug/net9.0-ios/iossimulator-x64/StudentFirestore.AssemblyInfo.cs
+++ StudentFirestore/obj/Debug/net9.0-ios/iossimulator-x64/StudentFirestore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentFirestore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+92694afb3c1d37c2093fce8b0ba2760472b2789f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+71730b630b41ebee377fd70c6f68977131699e44")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentFirestore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentFirestore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
